a29566ec2e1227eb1b909f7f01af9dc2616e8694
Thomas Thiele <thomas.thiele@kaeuferportal.de>
2013-05-05 11:56:43 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Testprojekt::Application.config.secret_token = '43fad8e512fbb356a6d3c8cbafe3f5a4f735d315997e7d6a3c1aa7e52bc229cccae9adae3463671fd3bf24276c26b5cb8b4f3192e70960c6f94606bf941c9f2c'
